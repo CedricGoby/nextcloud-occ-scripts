225f93c380f179cbc7b95861ae60e270bbcdff68
Cédric Goby <cedric.goby@inrae.fr>
2024-06-20 20:45:36 +0200

    Add apps

--- install_uninstall_enable_apps.sh
+++ install_uninstall_enable_apps.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-# Description : Installation, désinstallation, activation d'applications Nextcloud
+# Description : Installation, désinstallation, activation, désactivation d'applications Nextcloud
 # https://apps.nextcloud.com/
 # Usage : ./install_uninstall_enable_apps.sh
 # Licence : MIT
 # Auteur : Cédric Goby
 # Versioning : https://gitlab.com/CedricGoby/nextcloud-occ-scripts
 
-# Remove apps
+# REMOVE apps
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:remove firstrunwizard
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:remove dashboard
+docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:remove weather_status
+docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:remove survey_client
+docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:remove recommendations
 
-# Enable apps
+# ENABLE apps
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:enable files_external
 
-# Install apps
+# INSTALL apps
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install calendar
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install richdocuments
 docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install richdocumentscode
+docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install assistant
