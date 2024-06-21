e3f40e652933dabc9fae6bb4737b2ffa26abc433
Cédric Goby <cedric.goby@inrae.fr>
2024-06-21 10:59:18 +0200

    Create functions

--- install_uninstall_enable_apps.sh
+++ install_uninstall_enable_apps.sh
@@ -7,18 +7,58 @@
 # Auteur : Cédric Goby
 # Versioning : https://gitlab.com/CedricGoby/nextcloud-occ-scripts
 
-# DISABLE shipped apps
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable firstrunwizard
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable dashboard
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable weather_status
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable survey_client
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable recommendations
+# Liste des applications à désactiver
+disable_apps=("firstrunwizard" "dashboard" "weather_status" "survey_client" "recommendations")
 
-# ENABLE apps
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:enable files_external
+# Liste des applications à activer
+enable_apps=("files_external")
 
-# INSTALL apps
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install calendar
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install richdocuments
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install richdocumentscode
-docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install assistant
+# Liste des applications à installer
+install_apps=("calendar" "richdocuments" "richdocumentscode" "assistant")
+
+# Fonction pour désactiver les applications
+disable_apps() {
+  for app in "${disable_apps[@]}"
+  do
+    echo "Désactivation de l'application : $app"
+    result=$(docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:disable "$app" 2>&1)
+    if [[ $? -eq 0 ]]; then
+      echo "L'application $app a été désactivée avec succès."
+    else
+      echo "Erreur lors de la désactivation de l'application $app : $result"
+    fi
+  done
+}
+
+# Fonction pour activer les applications
+enable_apps() {
+  for app in "${enable_apps[@]}"
+  do
+    echo "Activation de l'application : $app"
+    result=$(docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:enable "$app" 2>&1)
+    if [[ $? -eq 0 ]]; then
+      echo "L'application $app a été activée avec succès."
+    else
+      echo "Erreur lors de l'activation de l'application $app : $result"
+    fi
+  done
+}
+
+# Fonction pour installer les applications
+install_apps() {
+  for app in "${install_apps[@]}"
+  do
+    echo "Installation de l'application : $app"
+    result=$(docker exec -it --user www-data nextcloud-app php /var/www/html/occ app:install "$app" 2>&1)
+    if [[ $? -eq 0 ]]; then
+      echo "L'application $app a été installée avec succès."
+    else
+      echo "Erreur lors de l'installation de l'application $app : $result"
+    fi
+  done
+}
+
+# Appeler les fonctions
+disable_apps
+enable_apps
+install_apps
